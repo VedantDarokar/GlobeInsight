3750369640f937c50cd42d055f6408936ad3f650
Vedant Darokar <149192548+VedantDarokar@users.noreply.github.com>
2025-02-28 09:34:09 +0530

    Initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export class Navbar extends Component {
     return (
       <nav className={`navbar navbar-expand-lg ${this.props.theme === 'dark' ? 'navbar-dark bg-dark' : 'bg-body-tertiary'}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">NewsMonkey</a>
+          <a className="navbar-brand" href="/">GlobeInsight</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
